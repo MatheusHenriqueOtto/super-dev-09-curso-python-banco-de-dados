13ab05b1495d0fd41c12444c176833ba003a49fb
matheus <matheusotto1906@gmail.com>
2026-08-16 21:53:30 -0300

    A parte do colaborador zero, realizada

--- src/run.py
+++ src/run.py
@@ -1,32 +1,42 @@
-from funcionario import menu_funcionario
+from funcionarios_1_1_0 import menu_funcionario
+from clientes import menu_cliente
+from pratos_feitos import menu_prato_feito
 import os
 
 
 def __main():
-    os.system("cls")
-    mensagem = """MENU:
+    os.system("cls" if os.name == "nt" else "clear")
+    mensagem = """
+=================================
+MENU:
+=================================
+
 1 - Funcionarios
 2 - Pratos feitos
 3 - Clientes
 4 - Bebidas
 5 - Mesas
-10 - Sair
+0 - Sair
 
 Digite a opção desejada: """
 
     opcao = int(input(mensagem))
 
-    while opcao != 10:
+    while opcao != 0:
         os.system("cls")
         if opcao == 1:
             menu_funcionario()
+        elif opcao == 2:
+            menu_prato_feito()
+        elif opcao == 3:
+            menu_cliente()
         elif opcao != 10:
             print("Opção invalida")
         print("\n")
 
         opcao = int(input(mensagem))
 
-    os.system("cls")
+    os.system("cls" if os.name == "nt" else "clear")
 
 if __name__ == "__main__":
     __main()
